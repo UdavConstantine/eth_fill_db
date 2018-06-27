b2216d0c7679f7002a87f619cb07e02536af7560
Konstantin Udavikhin <UdavConstantine@yandex.ru>
2018-06-28 00:02:04 +0300

    alfa

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ var vm = new Vue({
   data: {
       message: 'Hello, Vue!',
       blocks: [],
-      block: {}
+      block: {},
+      transactions: []
   },
   methods: {
       getBlocks: function () {
           axios
-              .get("http://localhost:3000/blocks?select=number,hash,txcount&order=number.desc&limit=10")
+              .get("http://localhost:3000/blocks?select=number,hash,txcount&order=number.desc&limit=10&txcount=gt.5")
               .then(response => (this.blocks = response.data));
       },
       getBlock: function (num) {
           axios
               .get("http://localhost:3000/blocks?select=*&number=eq." + num)
               .then(response => (this.block = response.data[0]));
+      },
+      getTxByHash: function (hash) {
+          axios
+              .get("http://localhost:3000/transactions?select=*&hash=eq." + hash)
+              .then(response => (this.transactions = response.data));
+      },
+      getTxsByBlockNum: function (num) {
+          axios
+              .get("http://localhost:3000/transactions?select=*&blocknumber=eq." + num)
+              .then(response => (this.transactions = response.data));
+      },
+      getTxsByParticipant: function (hash) {
+          axios
+              .get("http://localhost:3000/transactions?select=*&or=(from.eq." + hash + ",to.eq." + hash
+                  + ")&order=blocknumber.desc&limit=200")
+              .then(response => (this.transactions = response.data));
       }
+
   }
 })
